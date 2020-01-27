d9ab804b273f72cef87763dab42d32a2841475da
Reinaldo Moreira Del Fiaco <33691177+reinaldodelfiaco@users.noreply.github.com>
2020-01-27 09:03:43 -0300

    atualizações

--- js/principal.js
+++ js/principal.js
@@ -3,32 +3,44 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
 // chama as variáveis no html
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = document.querySelector(".info-peso");
-var peso = tdPeso.textContent;
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+var paciente = document.querySelectorAll(".paciente");
 
-var tdIMC = paciente.querySelector(".info-imc");
+for(var i = 0; i < pacientes.lenght ; i++){
 
-// variáveis de restrição do modelo
-var pesoEhValido = true;
-var alturaEhValida = true;
+    var paciente = pacientes[i];
 
-if(peso <= 0 || peso >= 150) {
-    alert("Peso inválido");
-    pesoEhValido = false;
-    tdIMC.textContent = "Peso Inválido";
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+    
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if(peso <= 30 || peso >= 150){
+        console.log("Peso inválido!");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido";
+        paciente.style.color = "red";
+    }
+
+    if(altura <= 1.40 || altura >= 2.15){
+        console.log("Altura inválida!");
+        alturaEhValida = false;
+        tdAltura.textContent = "Altura inválida;"
+    }
+
+    // calculo do IMC
+    if(pesoEhValido && alturaEhValida) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
 }
 
-if(altura <= 0 || altura >= 3.00) {
-    alert("Altura inválida");
-    alturaEhValida = false;
-    tdAltura.textContent = "Altura Inválida";
-}
-
-// calculo do IMC
-if(alturaEhValida && pesoEhValido) {
-    var imc = peso / (altura * altura);
-tdIMC.textContent = imc;
-}
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click",function(){
+    console.log("cliquei no botão");
+})
